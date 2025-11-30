2b22e3ca56c1eb2af0fe136a2a8e17e98473ef37
Mansi-Kundariya <mansi.kundariya2612@gmail.com>
2025-11-30 15:45:16 +0530

    [feat] refresh and access token

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { type Application } from "express";
 import authRoutes from "./routes/auth.routes";
 import blogRoutes from "./routes/blog.routes";
+import cors from "cors";
+import cookieParser from "cookie-parser"; // for parsing cookies
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {
   res.send("API is running with TypeScript 🚀");
